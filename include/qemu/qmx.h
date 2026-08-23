4853f828419b6a4a47a57143526deae0e1dabe44
Aaron <179076459+StochasticEagle@users.noreply.github.com>
2026-08-22 23:18:26 -0600

    qmx: expose tolerant media bookkeeping

--- include/qemu/qmx.h
+++ include/qemu/qmx.h
@@ -11,4 +11,10 @@
 
 bool qmx_expand_argv(int *argc, char ***argv, Error **errp);
 
+/* Runtime bookkeeping used only for QMX-defined tolerant media. */
+void qmx_mark_drive_failed(const char *id);
+bool qmx_drive_failed(const char *id);
+const char *qmx_drive_path(const char *id);
+bool qmx_should_skip_device(const char *device_id, const char **drive_id);
+
 #endif /* QEMU_QMX_H */
